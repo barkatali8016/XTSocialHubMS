d787858bcc96e7e1c651cfe660ac4b411884002a
Barkat Ali <barkatali8016@gmail.com>
2022-08-12 13:52:32 +0530

    added

--- posts/src/index.ts
+++ posts/src/index.ts
@@ -1,11 +1,11 @@
-import express, { Express, Request, Response } from 'express';
-import 'dotenv/config'
+import express, { Application, Request, Response } from "express";
+import "dotenv/config";
 
-const app: Express = express();
+const app: Application = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
