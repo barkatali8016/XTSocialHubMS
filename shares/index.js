bd37e9617defdcf241bd416e31876969209799d8
Anant Jain <anajain6@publicisgroupe.net>
2022-08-12 14:19:30 +0530

    DB connection

--- shares/index.js
+++ shares/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
+const dotEnv = require('dotenv');
+
 var app = express();
-const port = 8080;
+
+const port = 5000;
+
+const connectDB = require('./config/db');
 
 const share_post_router = require('./routes/share-post');
 
-app.listen(port, ()=> console.log('API running'));
+app.listen(port, ()=> console.log(`Share API running on port: ${port}`));
+
+dotEnv.config();
+connectDB();
 
 app.use('/share', share_post_router);
 
