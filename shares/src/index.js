a1c683a5837f062b8d81d4bbca405397fa571989
layzanant <63229711+layzanant@users.noreply.github.com>
2022-08-17 14:46:42 +0530

    Anant feature (#4)
    * project structure
    
    * updated package.json
    
    * DB connection
    
    * router configuration
    
    * added 2 more routes to get users and sharecount
    
    * folder structure modified
    
    Co-authored-by: Anant Jain <anajain6@publicisgroupe.net>
    Co-authored-by: Jaswant Khati <jaskhati@publicisgroupe.net>

--- shares/src/index.js
+++ shares/src/index.js
@@ -8,11 +8,13 @@ const port = 5000;
 const connectDB = require('./config/db');
 
 const share_post_router = require('./routes/share-post');
+const share_count_router = require('./routes/share-count');
+const shared_users_router = require('./routes/shared-users');
 
 app.listen(port, ()=> console.log(`Share API running on port: ${port}`));
 
 dotEnv.config();
 connectDB();
 
-app.use(share_post_router);
+app.use([share_post_router, share_count_router, shared_users_router]);
 
