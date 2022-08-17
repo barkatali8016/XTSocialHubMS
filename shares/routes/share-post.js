bb490f93beb0b6deb591954f1cc3e93633528d85
Jaswant Khati <jaskhati@publicisgroupe.net>
2022-08-17 12:58:21 +0530

    added 2 more routes to get users and sharecount

--- shares/routes/share-post.js
+++ shares/routes/share-post.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* Share a post. */
-router.post('/api/post/:postId/share', async (req, res)=>{
+router.post('/post/:postId/share', async (req, res)=>{
   try{
     const postId = req.params.postId;
     res.send(`Post with postId: ${postId} shared successfully.`);
@@ -11,5 +11,42 @@ router.post('/api/post/:postId/share', async (req, res)=>{
   }
 });
 
+/* Get Share Count By PostId. */
+router.get('/post/:postId/shareCount', async (req, res)=>{
+  try{
+    const postId = req.params.postId;
+    const dummyCount = 5
+    res.send(`Post with postId ${postId} is been shared ${dummyCount} times.`);
+  } catch(error){
+    console.error(`Error: ${error}`);
+  }
+});
+
+/* Get Users who have shared the post by postId. */
+router.get('/post/:postId/shared/users', async (req, res)=>{
+  try{
+    const postId = req.params.postId;
+    const dummyUsers = [
+      {
+        name : 'Jaswant',
+        email : 'jaswant.khati@publicissapient.com'
+      },
+      {
+        name : 'Anant',
+        email : 'anant@publicissapient.com'
+      },
+      {
+        name : 'Jayesh',
+        email : 'jayesh@publicissapient.com'
+      }
+    ] 
+    res.send({
+      postId,
+      dummyUsers
+    });
+  } catch(error){
+    console.error(`Error: ${error}`);
+  }
+});
 
 module.exports = router;
