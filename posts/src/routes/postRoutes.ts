255cd74c67d458a443983aaa457dd8f6ceae31b8
Antony Jeban <antonyjeban@gmail.com>
2022-08-28 14:24:05 +0530

    Made some small changes to the routes

--- posts/src/routes/postRoutes.ts
+++ posts/src/routes/postRoutes.ts
@@ -1,7 +1,7 @@
 import { Express } from "express";
 import { PostController } from "../controller";
 
-import { CalculateReadingTime, PublishMessage } from "../utils";
+import { CalculateReadingTime, PublishMessage, STATUS_CODES } from "../utils";
 import { configuration } from "../config";
 import { multerInstance, PostsAuth } from "./middlewares";
 import { MulterError } from "multer";
@@ -21,8 +21,7 @@ export const postRoutes = async (app: Express, channel: any) => {
         USER_BINDING_KEY,
         JSON.stringify({ event: "POST_ADDED", data: { ...result } })
       );
-      res.statusCode = 201;
-      res.send(result);
+      res.status(STATUS_CODES.OK).send(result);
     } catch (error) {
       console.log(error);
       next(error);
@@ -32,8 +31,7 @@ export const postRoutes = async (app: Express, channel: any) => {
   app.get("/api/post/list", PostsAuth, async (_, res, next) => {
     try {
       const result = await postController.getAllPosts();
-      res.statusCode = 200;
-      res.send(result);
+      res.status(STATUS_CODES.OK).send(result);
     } catch (error) {
       console.log(error);
       next(error);
@@ -42,38 +40,49 @@ export const postRoutes = async (app: Express, channel: any) => {
 
   app.get("/api/post/:id", PostsAuth, async (req, res, next) => {
     try {
+      if (!req.params.id) {
+        return res.status(STATUS_CODES.BAD_REQUEST).json({
+          error: 'Post ID is required',
+        });
+      }
       const result = await postController.getIndividualPost(req.params.id);
-      res.statusCode = 200;
-      res.send(result);
+      res.status(STATUS_CODES.OK).send(result);
     } catch (error) {
       console.log(error);
       next(error);
     }
   });
 
-  app.post("/api/post/image-upload", PostsAuth, function (req, res) {
+  app.post("/api/post/image-upload", (req, res) => {
     let message = 'success';
+    if (!req.file) {
+      return res.status(STATUS_CODES.BAD_REQUEST).json({
+        error: 'The request does not have an image',
+      });
+    }
     multerInstance(req, res, (err) => {
       if (!err) {
-        res.statusCode = 200;
-        return res.send({ message });
+        return res.status(STATUS_CODES.OK).send({ message });
       }
-      res.statusCode = 500;
       if (err instanceof MulterError && err.code === 'LIMIT_FILE_SIZE') {
         message = `Image size should be less than 2MB.`;
-        return res.send({ message });
+        return res.status(STATUS_CODES.INTERNAL_ERROR).send({ message });
       }
       message = 'Image should be of type png, jpg, jpeg and gif';
-      res.send({ message });
+      res.status(STATUS_CODES.INTERNAL_ERROR).send({ message });
     });
   });
 
   // THis is just a __private link__ for now for deleting the post
   app.delete('/api/post/:id', PostsAuth, async (req, res, next) => {
     try {
+      if (!req.params.id) {
+        return res.status(STATUS_CODES.BAD_REQUEST).json({
+          error: 'Post ID is required',
+        });
+      }
       const result = await postController.deleteIndividualPost(req.params.id);
-      res.statusCode = 200;
-      res.send(result);
+      res.status(STATUS_CODES.OK).send(result);
     } catch (error) {
       console.log(error);
       next(error)
--- posts/src/utils/app-errors.ts
+++ posts/src/utils/app-errors.ts
@@ -1,10 +1,10 @@
-// const STATUS_CODES = {
-//     OK: 200,
-//     BAD_REQUEST: 400,
-//     UN_AUTHORISED: 403,
-//     NOT_FOUND: 404,
-//     INTERNAL_ERROR: 500,
-// }
+export const STATUS_CODES = {
+    OK: 200,
+    BAD_REQUEST: 400,
+    UN_AUTHORISED: 403,
+    NOT_FOUND: 404,
+    INTERNAL_ERROR: 500,
+}
 
 // interface IErrorInformation {
 //     name: string;
